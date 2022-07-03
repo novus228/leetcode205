4a42f5288935bbb3e755ba875f7acf3e3ff11f1e
Tanisha Kashyap <62598929+novus228@users.noreply.github.com>
2022-07-03 22:19:59 +0530

    Time: 117 ms (73.47%), Space: 52.3 MB (24.34%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        for(long int i=0;i<nums.size();i++)
+        {
+            if(m[nums[i]]==1)
+            {
+                return true;
+            }
+            m[nums[i]]=1;
+        }
+        return false;
+        
+    }
+};
