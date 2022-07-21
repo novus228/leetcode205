30f40dcde4a09c799f94e70fea6782de31103b63
Tanisha Kashyap <62598929+novus228@users.noreply.github.com>
2022-07-21 23:14:57 +0530

    Time: 7 ms (7.08%), Space: 6.2 MB (52.87%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(long int i=0;i<s.size();i++)
+        {
+            if(isupper(s[i]))
+                s[i]=tolower(s[i]);
+        }
+        return s;
+        
+    }
+};
